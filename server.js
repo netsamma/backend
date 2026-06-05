ee20bac287f5ae186d4b98e7c6365b76153ce3b3
netsamma <netsamma@gmail.com>
2026-06-05 17:20:21 +0200

    login part 2

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 import http from 'node:http';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+
+const corsOptions = {
+  origin: '*', // Sostituisci con l'URL esatto del tuo frontend
+  methods: 'GET,POST,PUT,DELETE',  // Metodi HTTP consentiti
+  allowedHeaders: ['Content-Type', 'Aux\thorization'], // Header accettati (fondamentale per i Token JWT!)
+  optionsSuccessStatus: 200 
+};
+
+app.use(cors(corsOptions));
+
+const PORT = 3000;
+const SECRET_KEY = "chiave_segreta_per_i_token";
+
+const users = [
+  { username: "admin", password: "admin", role: "admin" },
+  { username: "marco_bianchi", password: "password456", role: "user" },
+  { username: "giulia_verdi", password: "password789", role: "user" },
+  { username: "luca_neri", password: "passwordabc", role: "moderator" },
+  { username: "elena_gialli", password: "passwordxyz", role: "user" }
+];
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
@@ -16,31 +37,19 @@ app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const user = users.find(u => u.username === username);
 
-    if (!user || !(await bcrypt.compare(password, user.password))) {
+    if(!user || password != user.password){
         return res.status(401).json({ message: "Credenziali errate" });
     }
+
+    // if (!user || !(await bcrypt.compare(password, user.password))) {
+    //     return res.status(401).json({ message: "Credenziali errate" });
+    // }
 
     // Genera il token valido per 1 ora
     const token = jwt.sign({ username: user.username }, SECRET_KEY, { expiresIn: '1h' });
     res.json({ token });
 });
 
-
-
-// http.createServer(function (req, res) {
-//     const headers = {
-//         'Access-Control-Allow-Origin': '*', 
-//         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-//         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-//         'Content-Type': 'application/json'
-//     };
-
-//     // if (req.method === 'OPTIONS') {
-//     //     res.writeHead(204, headers);
-//     //     res.end();
-//     //     return;
-//     // }
-
-//     res.writeHead(200, headers);
-//     res.end('Hello World 2!');
-// }).listen(8080);
+app.listen(PORT, () => {
+    console.log(`Server avviato su http://localhost:${PORT}`);
+});
